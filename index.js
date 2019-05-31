22ff0aa450e9efab579047f021996d311fc3ae94
Frol <artem.mihailovich@gmail.com>
2019-05-31 04:18:36 +0300

    Это реализация функции параллельного выполнения асинхронных операций в стиле callback

--- index.js
+++ index.js
@@ -2,6 +2,40 @@
  * @param {Function[]} operations
  * @param {Function} callback
  */
-module.exports = function (operations, callback) {
 
+ /*
+ *Это реализация функции параллельного выполнения
+ *асинхронных операций в стиле callback
+ */
+ 
+module.exports = function (operations, callback) {
+var arrResult = [];
+var end = operations.length;
+/*
+*end поможет определить что все асинхронные операции
+*завершились и пора вызывать финальный callback
+*/
+operations.map(start);//запускаем все асинхронные функции
+
+function start (iOper,i){
+    iOper(next);
+    /*
+    *функция next определена в start что бы видеть параметр i
+    */
+        function next (err, data) {
+            if (err) {
+                end = null;//если есть ошибка не допустим вызов 
+                // cb с результатом
+                callback(err, null);//запускаем cb с ошибкой
+            }else if (end) {
+                arrResult[i] = data;//если ошибки нет то запишем результат
+                //на соответствующее место в массиве
+                end = end-1;
+            } 
+
+            if (end === 0) callback(null, arrResult);
+            //после завершения всех асинхронных операций
+            //вызвали cb  с результирующим массивом
+        }
+}
 };
